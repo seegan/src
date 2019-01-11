93c066c0d6a3e92c606ac6da24a0a392944c5839
seegan <see.gan21@gmail.com>
2019-01-11 11:51:50 +0530

    delete

--- wp-content/themes/src/inc/admin/sales/delivery_billing.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    if( isset($_GET['action']) && $_GET['action'] == 'view' ) {
-        include( get_template_directory().'/inc/admin/sales/delivery/view.php' );
-    } else if( isset($_GET['action']) && $_GET['action'] == 'update') {
-        include( get_template_directory().'/inc/admin/sales/delivery/update.php' );
-    } else {
-        include( get_template_directory().'/inc/admin/sales/delivery/new.php' );
-    }
-?>
--- wp-content/themes/src/inc/admin/sales/return_billing.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    if( isset($_GET['action']) && $_GET['action'] == 'view' ) {
-        include( get_template_directory().'/inc/admin/sales/return/view.php' );
-    } else if( isset($_GET['action']) && $_GET['action'] == 'update') {
-        include( get_template_directory().'/inc/admin/sales/return/update.php' );
-    } else {
-        include( get_template_directory().'/inc/admin/sales/return/new.php' );
-    }
-?>
